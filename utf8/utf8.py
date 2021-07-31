2864a12889424518a7944df79aa2c2b59d8aaef5
Luciano Ramalho <luciano@ramalho.org>
2021-07-30 21:17:58 -0300

    added comments

--- utf8/utf8.py
+++ utf8/utf8.py
@@ -1,10 +1,10 @@
 """
 utf8.decode(…): The world's slowest UTF-8 decoder function.
 
-Crated only to demonstrate one (mis)use of pattern matching in Python.
+Code to demonstrate one (mis)use of pattern matching in Python.
 
-Also may be useful as executable pseudocode to explain how the UTF-8
-encoding/decoding algorithm works.
+Also may be useful as executable pseudocode to explain how the
+UTF-8 encoding/decoding algorithm works.
 
 Maybe one day Python can have a syntax for efficient pattern matching
 of bit patterns, like Erlang has:
@@ -44,18 +44,18 @@ def decode(octets: bytes) -> str:
             match bits:
                 case [0, *rest]:                    # 0xxx_xxxx
                     out_bits = rest                 # -> 7 bits
-                case [1, 1, 0, *rest]:              # 110x_xxxx
+                case [1, 1, 0, *head]:              # 110x_xxxx
                     tail = unpack(next(stream), 6)  # 10xx_xxxx
-                    out_bits = rest + tail          # -> 11 bits
-                case [1, 1, 1, 0, *rest]:           # 1110_xxxx
+                    out_bits = head + tail          # -> 11 bits
+                case [1, 1, 1, 0, *head]:           # 1110_xxxx
                     t0 = unpack(next(stream), 6)    # 10xx_xxxx
                     t1 = unpack(next(stream), 6)    # 10xx_xxxx
-                    out_bits = rest + t0 + t1       # -> 16 bits
-                case [1, 1, 1, 1, 0, *rest]:        # 1111_0xxx
+                    out_bits = head + t0 + t1       # -> 16 bits
+                case [1, 1, 1, 1, 0, *head]:        # 1111_0xxx
                     t0 = unpack(next(stream), 6)    # 10xx_xxxx
                     t1 = unpack(next(stream), 6)    # 10xx_xxxx
                     t2 = unpack(next(stream), 6)    # 10xx_xxxx
-                    out_bits = rest + t0 + t1 + t2  # -> 21 bits
+                    out_bits = head + t0 + t1 + t2  # -> 21 bits
                 case _:
                     raise ValueError(f'Invalid UTF-8 bit pattern: {pack(bits):_b}')
         except StopIteration:
@@ -69,20 +69,20 @@ def encode(text: str) -> bytes:
     for char in text:
         code = ord(char)
         if code < 0x80:
-            out.append(code)  # 0xxx_xxxx
+            out.append(code)                             # 0xxx_xxxx
         elif 0x80 <= code < 0x800:
-            head = code >> 6 | 0b1100_0000         # 110x_xxxx
-            tail = code & 0b11_1111 | 0b1000_0000  # 10xx_xxxx
+            head = code >> 6 | 0b1100_0000               # 110x_xxxx
+            tail = code & 0b11_1111 | 0b1000_0000        # 10xx_xxxx
             out.extend([head, tail])
         elif 0x800 <= code < 0x1_0000:
-            head = code >> 12 | 0b1110_0000               # 1110_xxxx
-            middle = code >> 6 & 0b11_1111 | 0b1000_0000  # 10xx_xxxx
-            tail = code & 0b11_1111 | 0b1000_0000         # 10xx_xxxx
-            out.extend([head, middle, tail])
+            head = code >> 12 | 0b1110_0000              # 1110_xxxx
+            body = code >> 6 & 0b11_1111 | 0b1000_0000   # 10xx_xxxx
+            tail = code & 0b11_1111 | 0b1000_0000        # 10xx_xxxx
+            out.extend([head, body, tail])
         else:
-            head = code >> 18 | 0b1111_0000               # 1111_0xxx
-            neck = code >> 12 & 0b11_1111 | 0b1000_0000   # 10xx_xxxx
-            middle = code >> 6 & 0b11_1111 | 0b1000_0000  # 10xx_xxxx
-            tail = code & 0b11_1111 | 0b1000_0000         # 10xx_xxxx
-            out.extend([head, neck, middle, tail])
+            head = code >> 18 | 0b1111_0000              # 1111_0xxx
+            neck = code >> 12 & 0b11_1111 | 0b1000_0000  # 10xx_xxxx
+            body = code >> 6 & 0b11_1111 | 0b1000_0000   # 10xx_xxxx
+            tail = code & 0b11_1111 | 0b1000_0000        # 10xx_xxxx
+            out.extend([head, neck, body, tail])
     return bytes(out)
